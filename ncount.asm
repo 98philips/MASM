8970ea3593aa3f001fe0149f675e68b7f4c6f933
philippaul1998 <pippu98@gmail.com>
2019-05-08 20:39:50 +0530

    d

--- ncount.asm
+++
@@ -1,7 +0,0 @@
-DATA SEGMENT
-    MSG1 DB 10,13,'Enter the numbers :  $'
-    MSG2 DB 10,13,'Enter the number to be searched :  $'
-    P1 LABEL BYTE
-    M1 DB 0FFH
-    P11 DB M1 DUP ('$')
-DATA ENDS 
